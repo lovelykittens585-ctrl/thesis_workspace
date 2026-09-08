63962200ab166a88e6d1d9a86be7f9e511952f6a
lovelykittens585-ctrl <lovelykittens585-ctrl@users.noreply.github.com>
2026-09-07 19:10:10 -0500

    Describe what changed

---
+++ defining.js
@@ -0,0 +1,47 @@
+  
+  html, body 
+  margin: 0;
+  padding: 0;
+
+canvas 
+  display;block;
+
+
+h1
+  font-family; Arial, Arial, serif;
+  font-size; 25;
+  text-align; left;
+  color; rgb(0, 0, 0);
+
+h2
+  font-family; Arial, Arial, serif;
+  font-size; 22;
+  text-align; left;
+  color; rgb(0, 0, 0);
+html, body
+p1
+  font-family; Arial-Narrow, Arial, serif;
+  font-size; 20;
+
+   
+    padding; 8;
+    margin-top; 70;
+    margin-bottom; 70;
+    margin-left; 70;
+    margin-right; 70;
+
+   
+  
+p2
+  background-color; rgb(255, 220, 241);
+
+
+html, body 
+       background-image; url('https://i.postimg.cc/bvr1zLGK/STRATEGY.jpg');
+    background-size; cover;
+    background-repeat; no-repeat;
+    background-attachment; fixed;
+    background-color; rgb(228, 80, 167);
+
+
+    
